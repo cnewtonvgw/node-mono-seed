47ecc5434746145f543c3923cf3732ca8fe18e47
cnewton <cameron.newton@vgw.co>
2019-02-18 13:22:35 +0800

    Add test example.

---
+++ src/server/main.spec.ts
@@ -0,0 +1,6 @@
+describe('Hello, server tests!', () => {
+
+    test('Hello world', () => {
+        expect(1).toEqual(1);
+    });
+});
---
+++ src/webapp/main.spec.ts
@@ -0,0 +1,6 @@
+describe('Hello, webapp tests!', () => {
+
+    test('Hello world', () => {
+        expect(1).toEqual(1);
+    });
+});
